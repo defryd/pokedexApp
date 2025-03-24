4600a0bc4f4676577ad53147009ec996d805eed9
‘Freddy <elipson39z@gmail.com>
2025-03-24 17:44:20 -0500

    homescreen and design

--- src/presentation/screens/home/HomeScreen.tsx
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -1,22 +1,50 @@
-import { View } from 'react-native'
-import { Button, Text } from 'react-native-paper'
+import { FlatList, StyleSheet, View } from 'react-native'
+import { Text } from 'react-native-paper'
 import { getPokemons } from '../../../actions/pokemons'
 import { useQuery } from '@tanstack/react-query'
+import { PokeballBg } from '../../components/ui/PokeballBg'
+import { globalTheme } from '../../../config/theme/global-theme'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import { PokemonCard } from '../../components/pokemons/PokemonCard'
 
 export const HomeScreen = () => {
+    const {top} = useSafeAreaInsets();
 
-    const { isLoading, data = [] } = useQuery({
+    const { isLoading, data: pokemons = [] } = useQuery({
         queryKey: ['pokemons'], 
         queryFn: () => getPokemons(0),
         staleTime: 1000 * 60 * 60,
     })
 
     return (
-        <View>
-            <Text>HomeScreen</Text>
-            <Button mode="contained" onPress={() => console.log('Pressed')}>
-                Press me
-            </Button>
+        <View style={globalTheme.globalMargin}>
+
+            <PokeballBg style={styles.imgPosition} />
+
+            <FlatList
+                data={pokemons}
+                keyExtractor={(pokemon, index) => `${pokemon.id}-${index}`}
+                numColumns={2}
+                style={{ paddingTop: top + 20 }}
+                ListHeaderComponent={() => (
+                    <Text variant="displayMedium">Pokédex</Text>
+                )}
+                renderItem={({ item }) => (
+                    <PokemonCard pokemon={item} />
+                )}
+                // onEndReachedThreshold={0.6}
+                // onEndReached={() => fetchNextPage()}
+                showsVerticalScrollIndicator={false}
+            />
         </View>
     )
 }
+
+
+const styles = StyleSheet.create({
+    imgPosition: {
+        position: 'absolute',
+        top: -100,
+        right: -100,
+    },
+})
